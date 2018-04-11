902cd55b26326231160cf954606d11aa34b15394
Florian Pawlik <pawlik@fbihome.de>
2018-04-11 19:22:49 +0200

    added ConfigParser

--- oskar.py
+++ oskar.py
@@ -3,12 +3,10 @@ import time
 import random
 import datetime
 import telepot
-#import requests
-#import configparser
-#from google.cloud import translate
-#from google.auth import app_engine
-
+import requests
+import configparser
 from googletrans import Translator
+
 
 def handle(msg):
 
@@ -18,23 +16,26 @@ def handle(msg):
     command = msg['text']
     translatedCommand = translator.translate(command).text
 
+    response = requests.post(config['Default']['URL'], data=translatedCommand)
 
     print 'got command \"%s\" from user \"%s\"' % (command, userName)
     print 'translated into english: %s' % translatedCommand
 
     for keyword in keywords:
-        if keyword in command:
+        if keyword in translatedCommand:
             bot.sendMessage(chat_id, str('Dann sehen wir mal, was Otto so im Angebot hat!'))
             break
     else: 
        bot.sendMessage(chat_id, str('Hallo ' + userName + ', ich freue mich, von dir zu hoeren!'))
 
 
-keywords = ['shoppen', 'einkaufen', 'kauf', 'shopping']
+keywords = ['shop', 'buy']
 translator = Translator()    
 
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-bot = telepot.Bot('509164385:AAE0_pOzH6fTPIWO7jc1zm1-08lbKqd55kM')
+bot = telepot.Bot(config['Bot']['token'])
 bot.message_loop(handle)
 
 print 'Listening'
