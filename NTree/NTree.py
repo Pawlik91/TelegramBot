7324a72347af1393c7117c0b3d9cbd14539406cc
Krenol <lauritz_yannick.groeger@stud.tu-darmstadt.de>
2018-04-12 04:56:56 +0200

    update

--- NTree/NTree.py
+++ NTree/NTree.py
@@ -3,6 +3,9 @@ from Node import Node
 nodes = {}
 
 def addNode(key, parentKey, hasParent=True):
+    if key in nodes:
+        return
+
     if parentKey in nodes and hasParent == True:
         parent = nodes[parentKey]
         node = Node()
@@ -11,6 +14,8 @@ def addNode(key, parentKey, hasParent=True):
         parent.children.append(node)
         dic = {key : node}
         nodes.update(dic)
+        return
+        
     if hasParent == False:
         node = Node()
         node.key = key
